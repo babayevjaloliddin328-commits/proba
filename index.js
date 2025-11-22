297649f3e6462ab197f843605b16324b14a5c8c7
babayevjaloliddin328-commits <babayevjaloliddin328@gmail.com>
2025-11-22 20:33:00 +0500

    todo number 1

--- index.js
+++ index.js
@@ -1,26 +1,13 @@
 const elForm = document.querySelector(".form");
-// const elInput = document.querySelector(".input");
+const elInput = document.querySelector(".input");
 const elDelete = document.querySelector(".delete");
 const elList = document.querySelector(".list");
+const list  = document.querySelector(".list");
+const text = document.querySelector(".todo-text");
+const add = document.querySelector(".add");
 
-
-elForm.addEventListener('submit'), (e)=>{
-    e.preventDefault();
-    const inputValue = elForm["input"].value.trim();
-    elList.innerHTML =`
-            <div class="item">
-            <p class="todo-text">${inputValue}</p>
-            <div class="fix">
-                <div><i class="ri-calendar-2-line"> </i>
-                <span>22.11.2025</span>
-            </div>
-            <i class="ri-mail-check-fill"></i>
-            <i class="ri-delete-bin-6-line"></i>
-            </div>
-        </div>
-    `
-    inputValue.textContent = "";
-    console.log('salom');
-
-}
-
+add.addEventListener("click", (addEvent)=> {
+    addEvent.preventDefault();
+    const value = elInput.value;
+    text.textContent = `${value}`;    
+});
